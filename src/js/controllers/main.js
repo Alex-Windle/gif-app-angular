0ca543e18ea8692b086d88aa77bb0b7bf9d01978
Alex Windle <alexwindle85@gmail.com>
2016-11-15 14:56:12 -0500

    Add POST method to send form.

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -1,12 +1,16 @@
 const SERVER_URL = 'https://class-server.herokuapp.com/collections/alex-gifs-app/'; 
 
-function GifController ($scope) {
-	$scope.gifs = []; 
+function GifController ($scope, $http) {
 
-	
-
+	$scope.addGif = function (gif) {
+		$http.post(SERVER_URL, gif).then(function(response){
+			console.log(response.data);
+		});
+	}
 
 }; 
 
-GifController.$inject = ['$scope'];
+GifController.$inject = ['$scope', '$http'];
 export { GifController }; 
+
+//$http.get('/someUrl', config).then(successCallback, errorCallback);
