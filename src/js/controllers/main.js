04b8bf5b6e49cfd6352bfe14d1de7b76e95fbc80
Alex Windle <alexwindle85@gmail.com>
2016-11-15 15:37:36 -0500

    Adds functionality to push all submissions to the page.

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -3,9 +3,18 @@ const SERVER_URL = 'https://class-server.herokuapp.com/collections/alex-gifs-app
 function GifController ($scope, $http) {
 $scope.gifs = []; 
 
+	function init(){
+		$http.get(SERVER_URL).then(function(response){
+			console.log('Gets server response --->', response.data);
+			$scope.gifs = response.data; 
+		});
+	};
+
+	init();
+
 	$scope.addGif = function (gif) {
 		$http.post(SERVER_URL, gif).then(function(response){
-			console.log(response.data); 
+			console.log('Posts form -->', response.data); 
 			
 			let gif = response.data;
 			$scope.gifs.push(gif); 
