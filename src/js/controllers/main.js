7b55911e1d594eeb447f835d866396bfdd5188d7
Alex Windle <alexwindle85@gmail.com>
2016-11-15 20:28:29 -0500

    Validates and submits name field.

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -2,24 +2,84 @@ const SERVER_URL = 'https://class-server.herokuapp.com/collections/alex-gifs-app
 
 function GifController ($scope, $http) {
 $scope.gifs = []; 
+$scope.errors = {}; 
 
 	function init(){
 		$http.get(SERVER_URL).then(function(response){
-			console.log('Gets server response --->', response.data);
 			$scope.gifs = response.data; 
 		});
 	};
 
-	init();
+	init(); 
+
+	$scope.validateName = function(name){
+		if (name === undefined || name === ''){
+			$scope.errors.name = "You must include a name."; 
+			return false; 
+		} else {
+			return true;
+		}
+
+	};
+
+	// $scope.validateURL = function(url){
+	// 	console.log('URL', url); 
+	// 	console.log('URL.url', url.url);
+	// 	if(!url.startsWith('http')){
+	// 		$scope.errors.url = "Must be a valid URL starting with http or https."; 
+	// 		return false; 
+	// 	}else{
+	// 		$scope.errors.url = ""; 
+	// 		return true;
+	// 	}
+	// }; 
+
+	// $scope.validateEmail = function(email){
+	// 	if(!email.includes('@')){
+	// 		$scope.errors.email = "Must be a valid email including @ symbol."; 
+	// 		return false;
+	// 	}else{
+	// 		$scope.errors.email = ""; 
+	// 		return true;
+	// 	}
+	// }; 
+
+	// $scope.validateMessage = function(message){
+	// 	if (message === undefined){
+	// 		$scope.errors.message = "You must include a message."; 
+	// 		return false; 
+	// 	} else {
+	// 		return true;
+	// 	}
+
+	// };
+
+	// $scope.validateForm = function (){
+	// 	if ($scope.validateName(gif.name) && $scope.validateURL(gif.url) 
+	// 		&& $scope.validateEmail(gif.email) && $scope.validateMessage(gif.message)) {
+	// 		return true;
+	// 	} else {
+	// 		return false;
+	// 	}
+	// }; 
 
 	$scope.addGif = function (gif) {
-		$http.post(SERVER_URL, gif).then(function(response){
-			console.log('Posts form -->', response.data); 
-			
-			let gif = response.data;
-			$scope.gifs.push(gif); 
-		});
-	}
+		if ($scope.validateName(gif.name)) {
+			$http.post(SERVER_URL, gif).then(function(response){
+				let gif = response.data;
+				$scope.gifs.push(gif); 
+			});
+		}; 
+	};
+
+	// $scope.deleteGif = function (){
+	// 	$http.delete(SERVER_URL + gif._id).then(function(response){
+	// 		console.log(response); 
+	// 		$scope.gifs = $scope.gifs.filter(function(x){
+	// 			return x._id !== gif._id;
+	// 		});
+	// 	}); 
+	// }; 
 
 }; 
 
